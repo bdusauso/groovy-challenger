84423fc50992212a469835b15c64d200f3329090
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2015-01-16 06:35:31 +0100

    Initial commit.

---
+++ src/main/groovy/be/codinsanity/sandbox/challenger/GroovyChallengerApplication.groovy
@@ -0,0 +1,12 @@
+package be.codinsanity.sandbox.challenger
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class GroovyChallengerApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run GroovyChallengerApplication, args
+    }
+}
---
+++ src/test/groovy/be/codinsanity/sandbox/challenger/GroovyChallengerApplicationTests.groovy
@@ -0,0 +1,16 @@
+package be.codinsanity.sandbox.challenger
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.SpringApplicationConfiguration
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+
+@RunWith(SpringJUnit4ClassRunner)
+@SpringApplicationConfiguration(classes = GroovyChallengerApplication)
+class GroovyChallengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
